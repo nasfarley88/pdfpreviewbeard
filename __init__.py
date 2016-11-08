6c715adf136bef262482a02a5388fe1b2de3b305
Nathanael Farley <nasfarley88@gmail.com>
2016-11-08 16:41:16 +0000

    Sending pdf preview is now asynchronous.

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import logging
 import re
 
 from telegram.ext import MessageHandler, Filters, CommandHandler
+from telegram.ext.dispatcher import run_async
 from skybeard.beards import Beard
 
 logger = logging.getLogger(__name__)
@@ -15,38 +16,35 @@ def is_pdf(message):
     except AttributeError:
         return False
 
+@run_async
+def send_pdf_preview(self, bot, update):
+    logger.info("Attempting to upload photo")
+    from pprint import pprint
+    pprint(update.message)
+    file_id = update.message.document.file_id
+    logger.info("Getting file from telegram")
+    pdf_tg_file = bot.getFile(file_id)
+
+    with tempfile.NamedTemporaryFile(suffix=".pdf") as pdf_file:
+        pdf_tg_file.download(pdf_file.name)
+        png_file_bytes = sp.check_output([
+            "/bin/pdftoppm",
+            "-singlefile",
+            "-png",
+            pdf_file.name
+        ])
+
+    update.message.reply_photo(png_file_bytes)
 
 class PdfPreviewBeard(Beard):
 
     def initialise(self):
         self.disp.add_handler(CommandHandler("pdfpreviewhelp", self.help))
-        # self.disp.add_handler(MessageHandler(Filters.document, self.send_pdf_preview))
-        self.disp.add_handler(MessageHandler(is_pdf, self.send_pdf_preview))
+        self.disp.add_handler(MessageHandler(is_pdf, send_pdf_preview))
 
     def help(self, bot, update):
         update.message.reply_text(
             """TL;DR This beard shows you the first page of any pdfs it sees."""
         )
 
-    def send_pdf_preview(self, bot, update):
-        logger.info("Attempting to upload photo")
-        from pprint import pprint
-        pprint(update.message)
-        file_id = update.message.document.file_id
-        logger.info("Getting file from telegram")
-        pdf_tg_file = bot.getFile(file_id)
-
-        with tempfile.NamedTemporaryFile(suffix=".pdf") as pdf_file:
-            pdf_tg_file.download(pdf_file.name)
-            png_file_bytes = sp.check_output([
-                "/bin/pdftoppm",
-                "-singlefile",
-                "-png",
-                pdf_file.name
-            ])
-
-        with tempfile.NamedTemporaryFile(suffix=".png") as png_file:
-            png_file.write(png_file_bytes)
-            png_file.seek(0)
-            update.message.reply_photo(png_file)
 
